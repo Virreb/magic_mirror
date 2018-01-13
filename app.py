a65ac772143ae31bb44ee48ebfac05854f43c06e
Victor Bäckman <victorbackman2@gmail.com>
2018-01-13 14:47:56 +0100

    first draft

---
+++ app.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+
+app = dash.Dash()
+
+app.layout = html.Div(children=[
+    html.H1(children='Hello Dash'),
+
+    html.Div(children='''
+        Dash: A web application framework for Python.
+    '''),
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True, port=5000)
