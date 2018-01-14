5590810c2101706198ece30b04dbab3da4c99cb3
Isak Hjortgren <isakhjortgren@gmail.com>
2018-01-14 18:47:02 +0100

    hekjhfkje

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import dash
 from dash.dependencies import Input, Output
 import dash_core_components as dcc
 import dash_html_components as html
-import time
 import datetime
 import plotly.graph_objs as go
 import numpy as np
